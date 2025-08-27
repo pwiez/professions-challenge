3fefdd68e9bed71351af4b3a17c767b55a62b353
rafaelvenetikides <rafavenetikides@gmail.com>
2025-08-27 08:46:03 -0300

    Initial Commit

---
+++ professions-challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  professions-challenge
+//
+//  Created by Rafael Venetikides on 27/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ professions-challenge/professions_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  professions_challengeApp.swift
+//  professions-challenge
+//
+//  Created by Rafael Venetikides on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct professions_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
