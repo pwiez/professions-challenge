591b17212a4efeba74830d02540c5414421ccb47
Rafael Venetikides <90425850+RafaVenetikides@users.noreply.github.com>
2025-09-02 10:01:06 -0300

    feat: Created enums for artifac model
    These enums will be used in the ArtifactModel struct. They compose the checkbox options used to fill the artifact form

---
+++ professions-challenge/Models/Enums/Category.swift
@@ -0,0 +1,32 @@
+//
+//  Category.swift
+//  professions-challenge
+//
+//  Created by Rafael Venetikides on 01/09/25.
+//
+
+import Foundation
+
+enum Category {
+    case artefact
+    case ecofact
+    case bioarcheological
+    case structure
+    case sediment
+    case archaeobotanical
+    case zooarchaeological
+    case other(description: String)
+    
+    var label: String {
+        switch self {
+        case .artefact: return "Artefato"
+        case .ecofact: return "Ecofato"
+        case .bioarcheological: return "Bioarqueológico"
+        case .structure: return "Estrutura/Feição"
+        case .sediment: return "Sedimento/Solo"
+        case .archaeobotanical: return "Arqueobotânico"
+        case .zooarchaeological: return "Zooarqueológico"
+        case .other(let description): return description
+        }
+    }
+}
---
+++ professions-challenge/Models/Enums/Decoration.swift
@@ -0,0 +1,48 @@
+//
+//  Decoration.swift
+//  professions-challenge
+//
+//  Created by Rafael Venetikides on 01/09/25.
+//
+
+import Foundation
+
+enum Decoration {
+    case smoothed
+    case burnished
+    case corrugated
+    case brushed
+    case ungulate
+    case incision
+    case impression
+    case plastic
+    case painted
+    case punction
+    case applique
+    case englobe
+    case stencil
+    case carving
+    case dontApply
+    case other(description: String)
+    
+    var label: String {
+        switch self {
+        case .smoothed: return "Alisado"
+        case .burnished: return "Brunido"
+        case .corrugated: return "Corrugado"
+        case .brushed: return "Escovado"
+        case .ungulate: return "Ungulado"
+        case .incision: return "Incisão"
+        case .impression: return "Impressão"
+        case .plastic: return "Plástica"
+        case .painted: return "Pintado"
+        case .punction: return "Punção"
+        case .applique: return "Aplique"
+        case .englobe: return "Engobe"
+        case .stencil: return "Estêncil"
+        case .carving: return "Entalhe"
+        case .dontApply: return "Não se aplica"
+        case .other(let description): return description
+        }
+    }
+}
