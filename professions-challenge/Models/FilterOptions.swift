bc68714c2b3132b72b4cc7818db711f39c1e055e
Rafael Venetikides <90425850+RafaVenetikides@users.noreply.github.com>
2025-09-04 10:18:49 -0300

    feat: Filter sheet
    This commit implements the filter sheet from the home view.
    For now, the filter has no functionality, as the app doesn't have a create records yet, but it's functionality should be trivial

---
+++ professions-challenge/Models/FilterOptions.swift
@@ -0,0 +1,15 @@
+//
+//  FilterOptions.swift
+//  professions-challenge
+//
+//  Created by Rafael Venetikides on 04/09/25.
+//
+
+import Foundation
+
+struct FilterOptions {
+    var exported = false
+    var recents = false
+    var withImage = false
+    var withAudio = false
+}
