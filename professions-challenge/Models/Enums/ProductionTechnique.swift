9627e1553a52b02fcb29fdd33fea21009a147ac0
rafaelvenetikides <rafavenetikides@gmail.com>
2025-09-08 18:07:25 -0300

    fix: Fixed project crashing
    The project was crashing due to attributes names conflicting with swiftData

--- professions-challenge/Models/Enums/ProductionTechnique.swift
+++ professions-challenge/Models/Enums/ProductionTechnique.swift
@@ -20,7 +20,7 @@ enum ProductionTechnique: Codable, Hashable, SelectableOption {
     case cast
     case forged
     case indefinite
-    case other(description: String)
+    case other(text: String)
     
     var label: String {
         switch self {
@@ -38,7 +38,7 @@ enum ProductionTechnique: Codable, Hashable, SelectableOption {
         case .cast: return "Fundido"
         case .forged: return "Forjado"
         case .indefinite: return "Indeterminado"
-        case .other(let description): return description
+        case .other(let text): return text
         }
     }
 }
